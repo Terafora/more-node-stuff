e0ab8fd321b46f8ab86b10cb36c7e8ca97720e21
Charlotte Stone <144109245+Terafora@users.noreply.github.com>
2023-12-21 14:02:40 +0000

    Add different responses for different paths

--- index.js
+++ index.js
@@ -3,13 +3,26 @@ const http = require('http');
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, {
-    'Content-Type': 'application/json'
-    });
-    res.end(JSON.stringify({
-        id: 1,
-        name: 'John Doe',    
-    }));
+    if (req.url === '/friends') {
+        res.writeHead(200, {
+            'Content-Type': 'application/json'
+        });
+        res.end(JSON.stringify({
+            id: 1,
+            name: 'John Doe',    
+        }));
+    } else if (req.url === '/messages') {
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<body>');
+        res.write('<ul>');
+        res.write('<li>Hello there!</li>');
+        res.write('<li>General Kenobi!</li>');
+        res.write('</ul>');
+        res.write('</body>');
+        res.write('</html>');
+        res.end();
+    }
 });
 
 server.listen(PORT, () => {
