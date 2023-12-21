344d86d1ef5ea904bced54c951bc37215ac2d898
Charlotte Stone <144109245+Terafora@users.noreply.github.com>
2023-12-21 15:20:15 +0000

    Final commit on this repo

--- index.js
+++ index.js
@@ -19,7 +19,14 @@ const friends = [
 
 const server = http.createServer((req, res) => {
     const items = req.url.split('/');
-    if (items[1] === 'friends') {
+    if (req.method === 'POST' && items[1] === 'friends') {
+        req.on('data', (data) => {
+            const friend = data.toString();
+            console.log('Request:', friend);
+            friends.push(JSON.parse(friend));
+        });
+        req.pipe(res);
+    } else if (req.method === 'GET' && items[1] === 'friends') {
         res.writeHead(200, {
             'Content-Type': 'application/json'
         });
@@ -29,7 +36,7 @@ const server = http.createServer((req, res) => {
         } else {
             res.end(JSON.stringify(friends));
         }
-    } else if (items[1] === 'messages') {
+    } else if (req.method === 'GET' && items[1] === 'messages') {
         res.setHeader('Content-Type', 'text/html');
         res.write('<html>');
         res.write('<body>');
